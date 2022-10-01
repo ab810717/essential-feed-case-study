44f57d52dfd15dfe5ec7326ea6f1e7ed7432ec42
andy hau <andy.hau@acaia.co>
2022-10-01 14:54:53 +0800

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Andy Hao on 2022/10/1.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL?
+}
