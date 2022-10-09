f5635a113d8174c2469f1297150d779891d1b47b
andy hau <andy.hau@acaia.co>
2022-10-10 02:44:58 +0800

    Move `URLSessionHTTPClient` to its own file in production

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -8,27 +8,6 @@
 import XCTest
 import EssentialFeed
 
-class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    struct UnexpectedValueRepresntation: Error {}
-    
-    func get(from url: URL, completion: @escaping  (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { data, response , error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.succses(data, response))
-            } else {
-                completion(.failure(UnexpectedValueRepresntation()))
-            }
-        }.resume()
-    }
-}
 
 class URLSessionHTTPClientTests: XCTestCase {
     override func setUp() {
